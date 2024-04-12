46cbd2a9913732403def19da001fa79a6aae0ab5
mat <162229435+IgorLeite12@users.noreply.github.com>
2024-04-11 20:59:57 -0400

    aula1104

---
+++ Aula_11-04/calculadora.py
@@ -0,0 +1,30 @@
+# O código abaixo tem alguns problemas e está incompleto! 
+# Altere o código abaixo para que ele atue como uma calculadora
+# O usuário deve digitar um valor, um operador e outro valor
+# seu programa deverá imprimir na tela o resultado da operação
+# Faça com que o seu programa funcione até que o usuário digite -1
+
+valor1 = int(input("Digite um:"))
+valor2= int(input("Digite outro:"))
+
+while valor != '-1' and valor2 != -1
+    print(valor("Digite outro número ou escreva '-1' para calcular:"))
+
+operador = input("Qual a operação? ('+', '-', '*', '/')")
+    
+
+    if operador == '+':
+        soma = valor1 + valor2
+        print("Reslutado:", soma)
+
+    elif operador == '-':
+        menos = valor1 - valor2
+        print("Reslutado:", menos)
+
+    elif operador == '*':
+        mult = valor1 * valor2
+        print("Reslutado:", mult)
+
+    elif operador == '/':
+        div = valor1 / valor2
+        print("Reslutado:", div)
---
+++ Aula_11-04/teste.py
@@ -0,0 +1,7 @@
+n = int(input())
+m = int(input())
+
+ while n != '0'
+    input(n)
+
+ soma = m + n
